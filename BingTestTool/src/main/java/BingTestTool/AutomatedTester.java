60cad3d16ce0eceaeb409a921e331ef55a1b9e08
jn432 <jn432@uowmail.edu.au>
2021-05-09 21:49:06 +1000

    Added evaluateResult method for TTD, added comments for runTest on structure

--- BingTestTool/src/main/java/BingTestTool/AutomatedTester.java
+++ BingTestTool/src/main/java/BingTestTool/AutomatedTester.java
@@ -11,12 +11,24 @@ public class AutomatedTester {
         this.maxScore = 0;
     }
     
+    //Evaluate whether the search result matches what our query is looking for
+    protected boolean evaluateResult(String querry, String resultTitle) {
+        //Unsure on method, discuss with team
+        return false;
+    }
+    
     //Run a test - return true if results are good, false if they are not
     //Increments curScore and maxScore on whether test passes/fails
     public boolean runTest() {
-        this.curScore++;
+        //generate words
+        //search queries using Bing
+        //compare search query to search results
+        boolean result = this.evaluateResult("blah","blah");
+        //add 1 if result was success, otherwise add 0
+        this.curScore += (result ? 1 : 0);
+        //always increment the max score
         this.maxScore++;
-        return true;
+        return result;
     }
     
     //method to run a set amount of tests
