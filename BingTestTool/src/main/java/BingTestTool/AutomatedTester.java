080640aba9c7537a5dfab457d9054c4c9b093f17
jn432 <jn432@uowmail.edu.au>
2021-05-14 21:01:43 +1000

    Added really basic evaluation and another test method(one test currently fails)

--- BingTestTool/src/main/java/BingTestTool/AutomatedTester.java
+++ BingTestTool/src/main/java/BingTestTool/AutomatedTester.java
@@ -1,34 +1,67 @@
 package BingTestTool;
 
+import java.util.ArrayList;
+
 public class AutomatedTester {
     
-    protected int curScore;
-    protected int maxScore;
+    private int curScore;
+    private int maxScore;
+    private int testsPassed;
+    private int totalTests;
     
     //Constructor
     public AutomatedTester() {
         this.curScore = 0;
         this.maxScore = 0;
+        this.testsPassed = 0;
+        this.totalTests = 0;
     }
     
     //Evaluate whether the search result matches what our query is looking for
-    protected boolean evaluateResult(String querry, String resultTitle) {
+    private boolean evaluateResult(String query, SearchResult result) {
         //Unsure on method, discuss with team
-        return false;
+        //returns true if link, title or description contains the word
+        //otherwise it returns false
+        return result.getLink().toLowerCase().contains(query.toLowerCase()) || 
+                result.getTitle().toLowerCase().contains(query.toLowerCase()) ||
+                result.getDescription().toLowerCase().contains(query.toLowerCase());
     }
     
     //Run a test - return true if results are good, false if they are not
     //Increments curScore and maxScore on whether test passes/fails
     public boolean runTest() {
         //generate words
+        WordGenerator wordGen = new WordGenerator();
+        String word = wordGen.generateWord();
+        
         //search queries using Bing
+        BingParser bp = new BingParser(word);
+        ArrayList<SearchResult> results = bp.getSearchTitles();
+        
+        //SCORING SECTION - VERY BASIC
+        int score = 0;
+        
         //compare search query to search results
-        boolean result = this.evaluateResult("blah","blah");
-        //add 1 if result was success, otherwise add 0
-        this.curScore += (result ? 1 : 0);
+        for (SearchResult result : results) {
+            boolean test = this.evaluateResult(word, result);
+            //SCORING SECTION - VERY BASIC
+            //add 1 if result was success, otherwise add 0
+            score += (test ? 1 : 0);
+        }
+        
+        //add points
+        this.curScore += score;
+        this.maxScore += results.size();
+        
+        //Counting number of tests passed
+        //add 1 if test scored at least 50%
+        this.testsPassed += ((score * 2 >= results.size()) ? 1 : 0);
         //always increment the max score
-        this.maxScore++;
-        return result;
+        this.totalTests++;
+        
+        //return true if tests passed
+        return (score * 2 >= results.size());
+        
     }
     
     //method to run a set amount of tests
@@ -47,4 +80,14 @@ public class AutomatedTester {
         return this.curScore/this.maxScore;
     }
     
+    public double getTestsPassedFraction() {
+        if (this.totalTests == 0) {
+            return 0;
+        }
+        return this.testsPassed/this.totalTests;
+    }
+    
+    public boolean evaluateResultTest(String query, SearchResult result) {
+        return evaluateResult(query, result);
+    }
 }
